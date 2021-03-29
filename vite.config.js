f805042ea9e4676a66bb3c76e1b36cc42d63843f
mateffet <ehuim5@gmail.com>
2021-03-29 22:55:08 +0000

    ...

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ module.exports = {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                nested: resolve(__dirname, 'host.html'),
+                main: resolve(__dirname, 'host.html')
                 // nested: resolve(__dirname, 'page/explore.html'),
                 // nested: resolve(__dirname, 'page/main-explore.html'),
                 // nested: resolve(__dirname, 'page/profile.html'),

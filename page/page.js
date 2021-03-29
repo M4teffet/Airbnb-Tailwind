8244336f45a4ee5e3796291d40713b7055ed7086
mateffet <ehuim5@gmail.com>
2021-03-29 23:29:50 +0000

    link refactoring

--- page/page.js
+++ page/page.js
@@ -1,5 +1,5 @@
-import './style.css'
+import '../style.css'
 
-import './public/assets/css/swiper-bundle.min.css'
+import '../public/assets/css/swiper-bundle.min.css'
 
-import './public/assets/js/swiper-bundle.min.js'
+import '../public/assets/js/swiper-bundle.min.js'

decff985894f885ffbcc85fea080427b9583192b
mateffet <ehuim5@gmail.com>
2021-03-30 00:53:10 +0000

    excluded profile

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 main: resolve(__dirname, 'index.html'),
                 host: resolve(__dirname, 'page/host.html'),
                 mainexplore: resolve(__dirname, 'page/main-explore.html'),
-                profile: resolve(__dirname, 'page/profile.html'),
+                // profile: resolve(__dirname, 'page/profile.html'),
                 explore: resolve(__dirname, 'page/explore.html'),
                 show: resolve(__dirname, 'page/show.html')
             }
